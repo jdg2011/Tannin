db33004dc57954a2ab964eb7266a64b4b965c974
Jordan Gloor <50277631+jdg2011@users.noreply.github.com>
2020-10-08 15:43:14 -0500

    Add files via upload

--- tannin.py
+++ tannin.py
@@ -5,52 +5,90 @@
 # Author:      Jordan Gloor
 #
 # Created:     08/10/2020
-# Copyright:   (c) gloorj 2020
-# Licence:     <your licence>
+# Copyright:   (c) Jordan Gloor 2020
+# Licence:     MIT License
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
 #-------------------------------------------------------------------------------
 
-#One helpful feature would be using a keyword for each entry, probably stored in a separate file. This would avoid reliance on proper order for retrieval and helps the user remember which entry is which. It would need to detect and reject previously used keywords.
+#Need to divide password input into two equal segments and store them in two separate files.
+#Eventually it should verify the input is exactly two 200 characters long.
+#Need to figure out how to ensure escape characters are handled properly.
 
 import os
+import datetime
 
 def greeting():
     print("-----------------------------------\rRunning Tannin Version 0.0.1 Alpha\r-----------------------------------\r")
 
 def file_check():
+    global log_file
+    log_file = open("log.txt", "w")
+    log_file.write("Log file created "+str(datetime.datetime.now())+"\r")
     if os.path.exists("tannin_keys.txt"):
-        print("Key directory detected!")
+        log_file.write(str(datetime.datetime.now())+" Key directory detected!\r")
     else:
-        print("Creating new key directory file...")
-    if os.path.exists("tannin_passwords.txt"):
-        print("Password directory detected!")
+        log_file.write(str(datetime.datetime.now())+" Creating new key directory file...\r")
+    if os.path.exists("tannin_passwords1.txt"):
+        log_file.write(str(datetime.datetime.now())+" Password directory 1 detected!\r")
+    if os.path.exists("tannin_passwords2.txt"):
+        log_file.write(str(datetime.datetime.now())+" Password directory 2 detected!\r")
     else:
-        print("Creating new password directory file...")
+        log_file.write(str(datetime.datetime.now())+" Creating new password directory files...\r")
 
 def select_task():
-    selected_task = str(input("Enter <s> to store <r> to retrieve <h> to get help: "))
-    if selected_task == "s":
-        print("Beginning storage protocol...")
-        store()
-    elif selected_task == "r":
-        print("Beginning retrieval protocol...")
-    elif selected_task == "h": print("Help page not yet created!")
-    else:
-        print("\""+selected_task+"\"Bad input. Try again.")
+    x = 0
+    while x == 0:
+        selected_task = str(input("Enter <s> to store <r> to retrieve <h> to get help: "))
+        if selected_task == "s":
+            log_file.write(str(datetime.datetime.now())+" Beginning storage protocol...\r")
+            x = 1
+            store()
+        elif selected_task == "r":
+            log_file.write(str(datetime.datetime.now())+" Beginning retrieval protocol...\r")
+            print("Retrieval function not yet implemented!")
+            continue
+        elif selected_task == "h":
+            print("Help page not yet created!")
+            continue
+        else:
+            print("\""+selected_task+"\" bad input. Try again.")
+            continue
+
 
 def store():
     keyword_to_store = str(input("Enter a keyword: "))
     password_to_store = str(input("Enter a password to be stored: "))
-    print("Storing keyword...")
-    keyword_f = open("tannin_keys.txt", "a")
-    keyword_f.write(keyword_to_store+"\r")
-    keyword_f.close()
-    print("Storing password...")
-    password_f = open("tannin_passwords.txt", "a")
-    password_f.write(password_to_store+"\r")
-    password_f.close()
+    log_file.write(str(datetime.datetime.now())+" Storing keyword <"+keyword_to_store+">\r")
+    keyword_file = open("tannin_keys.txt", "a")
+    keyword_file.write(keyword_to_store+"\r")
+    keyword_file.close()
+    log_file.write(str(datetime.datetime.now())+" Storing password <"+password_to_store+">\r")
+    password_file1 = open("tannin_passwords1.txt", "a")
+    password_file1.write(password_to_store[0]+"\r")
+    password_file1.close()
+    password_file2 = open("tannin_passwords2.txt", "a")
+    password_file2.write(password_to_store[1]+"\r")
+    password_file2.close()
     print("File writing complete!")
+    log_file.write(str(datetime.datetime.now())+" File writing complete!")
 
 greeting()
 file_check()
 select_task()
-#store()
+log_file.close()
