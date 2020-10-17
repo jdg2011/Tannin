483bbd66852c6bf3ebd7f5b5e72ce607a92535a0
Jordan Gloor <jdg120746@protonmail.com>
2020-10-17 10:31:05 -0500

    Changed wordage from "password" to "hash"

--- tannin.py
+++ tannin.py
@@ -1,6 +1,6 @@
 #-------------------------------------------------------------------------------
 # Name:        Tannin
-# Purpose:     To store passwords encrypted with Tea.lock in a disintegrated fashion so as to decrease malicious accessibility of stored passwords while maintaining ease of use.
+# Purpose:     To store hashes encrypted with Tea.lock in a disintegrated fashion so as to decrease malicious accessibility of stored hashes while maintaining ease of use.
 #
 # Author:      Jordan Gloor
 #
@@ -27,9 +27,9 @@
 #-------------------------------------------------------------------------------
 
 #Need to figure out how to ensure escape characters are handled properly.
-#The file_check function should eventually warn users when only one (non-empty) password library is present and ask what they'd like to do (retry, overwrite, quit)
+#The file_check function should eventually warn users when only one (non-empty) hash library is present and ask what they'd like to do (retry, overwrite, quit)
 #The storage process should also verify that the given keyword has not already been used
-#Need to get rid of the "what_now" operation in retrieval function and instead make copy command always available (just copy whatever password was last found) and it should also be a retrieval option.
+#File check should happen at the start of every command prompt
 
 tannin_version = "0.2.1 \"Stay\""
 
@@ -52,10 +52,10 @@ def file_check():
 	else:
 		log_file.write(str(datetime.datetime.now())+" Key directory not found. New file will be created.\r")
 		file_status = "bad"
-	if os.path.exists("passwords1.txt") and os.path.exists("passwords2.txt"):
-		log_file.write(str(datetime.datetime.now())+" Password directories found.\r")
+	if os.path.exists("hashes1.txt") and os.path.exists("hashes2.txt"):
+		log_file.write(str(datetime.datetime.now())+" hash directories found.\r")
 	else:
-		log_file.write(str(datetime.datetime.now())+" Password directories not found. New file will be created.\r")
+		log_file.write(str(datetime.datetime.now())+" hash directories not found. New file will be created.\r")
 		file_status = "bad"
 
 def get_command():
@@ -91,26 +91,26 @@ def get_command():
 def store():
 	keyword_to_store = str(input("Enter a keyword: "))
 	if option1 == "p":
-		log_file.write(str(datetime.datetime.now())+" Attempting to pull password from clipboard...\r")
-		print("Getting password from clipboard...")
-		password_to_store = pyperclip.paste()
+		log_file.write(str(datetime.datetime.now())+" Attempting to pull hash from clipboard...\r")
+		print("Getting hash from clipboard...")
+		hash_to_store = pyperclip.paste()
 	else:
-		password_to_store = str(input("Enter a password to be stored: "))
-	if len(password_to_store) != 200:
-		print("Entered length is "+str(len(password_to_store))+". Password must equal 200 characters.\r")
-		log_file.write(str(datetime.datetime.now())+" User entered password not length 200.\r")
+		hash_to_store = str(input("Enter a hash to be stored: "))
+	if len(hash_to_store) != 200:
+		print("Entered length is "+str(len(hash_to_store))+". hash must equal 200 characters.\r")
+		log_file.write(str(datetime.datetime.now())+" User entered hash not length 200.\r")
 	else:
-		log_file.write(str(datetime.datetime.now())+" Successful keyword and password input. Storing keyword...\r")
+		log_file.write(str(datetime.datetime.now())+" Successful keyword and hash input. Storing keyword...\r")
 		keyword_file = open("keys.txt", "a")
 		keyword_file.write(keyword_to_store+"\r")
 		keyword_file.close()
-		log_file.write(str(datetime.datetime.now())+" Storing password...\r")
-		password_file1 = open("passwords1.txt", "a")
-		password_file1.write(password_to_store[0:100]+"\r")
-		password_file1.close()
-		password_file2 = open("passwords2.txt", "a")
-		password_file2.write(password_to_store[100:200]+"\r")
-		password_file2.close()
+		log_file.write(str(datetime.datetime.now())+" Storing hash...\r")
+		hash_file1 = open("hashes1.txt", "a")
+		hash_file1.write(hash_to_store[0:100]+"\r")
+		hash_file1.close()
+		hash_file2 = open("hashes2.txt", "a")
+		hash_file2.write(hash_to_store[100:200]+"\r")
+		hash_file2.close()
 		global file_status
 		file_status = "ok"
 		log_file.write(str(datetime.datetime.now())+" File writing complete!\r")
@@ -129,8 +129,8 @@ def keyword_query():
 			print("Keyword \""+keyword_to_find+"\" not found.\r")
 			keyword_file.close()
 	else:
-		log_file.write(str(datetime.datetime.now())+" One or more files are missing; password retrieval not possible.\r")
-		print("Files <keys.txt> <password1.txt> and <password2.txt> must be present and non-empty to initiate retrieval.\nEnsure all files are in Tannin directory or store a new password with command <s>.\n")
+		log_file.write(str(datetime.datetime.now())+" One or more files are missing; hash retrieval not possible.\r")
+		print("Files <keys.txt> <hash1.txt> and <hash2.txt> must be present and non-empty to initiate retrieval.\nEnsure all files are in Tannin directory or store a new hash with command <s>.\n")
 
 def line_finder(keyword_to_find):
 	keyword_file = open("keys.txt", "r")
@@ -142,34 +142,34 @@ def line_finder(keyword_to_find):
 						break
 
 def retrieval(key):
-	password_file1 = open("passwords1.txt", "r")
-	password_file2 = open("passwords2.txt", "r")
+	hash_file1 = open("hashes1.txt", "r")
+	hash_file2 = open("hashes2.txt", "r")
 	z = 0
 	for z in range(int(key)):
 		z += 1
 		if z == key:
-			global found_password
-			found_password = password_file1.readline().rstrip('\n')+password_file2.readline().rstrip('\n')
-			print("Password found:\n\n--------------------------\n"+found_password+"\n--------------------------")
-			log_file.write(str(datetime.datetime.now())+" Password successfully delivered.\r")
-			password_file1.close()
-			password_file2.close()
+			global found_hash
+			found_hash = hash_file1.readline().rstrip('\n')+hash_file2.readline().rstrip('\n')
+			print("Hash found:\n\n--------------------------\n"+found_hash+"\n--------------------------")
+			log_file.write(str(datetime.datetime.now())+" Hash successfully delivered.\r")
+			hash_file1.close()
+			hash_file2.close()
 		else:
-			log_file.write(str(datetime.datetime.now())+" Cycling through password files...\r")
-			password_file1.readline()
-			password_file2.readline()
+			log_file.write(str(datetime.datetime.now())+" Cycling through hash files...\r")
+			hash_file1.readline()
+			hash_file2.readline()
 			continue
 	if option1 == "c":
-		copy_password()
+		copy_hash()
 
-def copy_password():
-	if found_password != 0:
-		log_file.write(str(datetime.datetime.now())+" Copying found password to clipboard...\r")
-		pyperclip.copy(found_password)
-		print("Password copied to clipboard.")
+def copy_hash():
+	if found_hash != 0:
+		log_file.write(str(datetime.datetime.now())+" Copying found hash to clipboard...\r")
+		pyperclip.copy(found_hash)
+		print("Hash copied to clipboard.")
 	else:
-		log_file.write(str(datetime.datetime.now())+" User attempted copying password when no password found.\r")
-		print("Find a password first with command <r>")
+		log_file.write(str(datetime.datetime.now())+" User attempted copying hash when no hash found.\r")
+		print("Find a hash first with command <r>")
 
 def print_help():
 	if os.path.exists("help.txt"):
@@ -200,18 +200,18 @@ def wipe_files():
 	else:
 		log_file.write(str(datetime.datetime.now())+" Keyword file not found.\r")
 		print("Keyword file does not exist.")
-	if os.path.exists("passwords1.txt"):
-		log_file.write(str(datetime.datetime.now())+" Deleting first password file...\r")
-		os.remove("passwords1.txt")
+	if os.path.exists("hashes1.txt"):
+		log_file.write(str(datetime.datetime.now())+" Deleting first hash file...\r")
+		os.remove("hashes1.txt")
 	else:
-		log_file.write(str(datetime.datetime.now())+" First password file not found.\r")
-		print("First password file does not exist.")
-	if os.path.exists("passwords2.txt"):
-		log_file.write(str(datetime.datetime.now())+" Deleting second password file...\r")
-		os.remove("passwords2.txt")
+		log_file.write(str(datetime.datetime.now())+" First hash file not found.\r")
+		print("First hash file does not exist.")
+	if os.path.exists("hashes2.txt"):
+		log_file.write(str(datetime.datetime.now())+" Deleting second hash file...\r")
+		os.remove("hashes2.txt")
 	else:
-		log_file.write(str(datetime.datetime.now())+" Second password file not found.\r")
-		print("Second password file does not exist.")
+		log_file.write(str(datetime.datetime.now())+" Second hash file not found.\r")
+		print("Second hash file does not exist.")
 	print("File wiping complete.\n")
 	log_file.write(str(datetime.datetime.now())+" File wiping complete.\r")
 
@@ -232,7 +232,7 @@ def task(selected_task):
 		list_keys()
 	elif selected_task == "copy":
 		log_file.write(str(datetime.datetime.now())+" Beginning copy protocol...\r")
-		copy_password()
+		copy_hash()
 	elif selected_task == "quit":
 		log_file.write(str(datetime.datetime.now())+" Quitting...\r")
 		global T
@@ -240,7 +240,7 @@ def task(selected_task):
 
 greeting()
 file_check()
-found_password = 0
+found_hash = 0
 T = 0
 while T == 0:
 	option1 = 0
